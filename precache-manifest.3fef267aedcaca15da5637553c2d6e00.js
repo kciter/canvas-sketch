a28bd1e90887b76eb18719e0a2b339a17c1892b3
Lee Sun-Hyoup <kciter@naver.com>
2022-11-13 00:33:43 +0900

    Updates

---
+++ precache-manifest.3fef267aedcaca15da5637553c2d6e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95aff52e0cf7bd57195f53ad80521682",
+    "url": "/canvas-sketch/index.html"
+  },
+  {
+    "revision": "ceadc9ba05290df38fff",
+    "url": "/canvas-sketch/static/css/main.5650c96a.chunk.css"
+  },
+  {
+    "revision": "545f84d7dfe553f05d40",
+    "url": "/canvas-sketch/static/js/2.42f83962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canvas-sketch/static/js/2.42f83962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceadc9ba05290df38fff",
+    "url": "/canvas-sketch/static/js/main.cff06e4e.chunk.js"
+  },
+  {
+    "revision": "5a10ff493a448271e3a7",
+    "url": "/canvas-sketch/static/js/runtime-main.ba2efac7.js"
+  }
+]);
